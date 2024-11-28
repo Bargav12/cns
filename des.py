64bec8c4ebb0ff6045b3ece02edbf4968bcca01a
G V N S Bhargav <144159609+Bargav12@users.noreply.github.com>
2024-11-28 20:34:19 +0530

    Update des.py

--- des.py
+++ des.py
@@ -1,4 +1,45 @@
 from Crypto.Cipher import DES
+from Crypto.Random import get_random_bytes
+
+# Padding function
+def pad(text):
+    while len(text) % 8 != 0:
+        text += ''
+    return text
+
+def main():
+    message = "This is a confidential message."
+    message = pad(message)
+    my_message = message.encode('utf-8')
+
+    # Generate a random DES key (8 bytes)
+    my_des_key = get_random_bytes(8)
+
+    # Create DES cipher in ECB mode
+    my_cipher = DES.new(my_des_key, DES.MODE_ECB)
+
+    # Encrypt the message
+    my_encrypted_bytes = my_cipher.encrypt(my_message)
+
+    # Decrypt the message using the same key and cipher mode
+    my_cipher = DES.new(my_des_key, DES.MODE_ECB)
+    my_decrypted_bytes = my_cipher.decrypt(my_encrypted_bytes)
+
+    encrypted_data = my_encrypted_bytes
+    decrypted_data = my_decrypted_bytes.decode('utf-8').strip()
+
+    print("Message: ", message.strip())
+    print("Encrypted (in bytes): ", encrypted_data)
+    print("Decrypted Message: ", decrypted_data)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+from Crypto.Cipher import DES
 from secrets import token_bytes
 
 # Function to pad the text to ensure it's a multiple of 8 bytes
